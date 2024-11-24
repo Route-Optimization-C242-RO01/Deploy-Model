cb01073c3324fda843a61e42df99a47dbbf19ca6
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-11-24 19:50:00 +0700

    Update model.py

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ from collections import OrderedDict
 import requests
 app = Flask(__name__)
 api = Api(app)
+import os
+
 
 # Fungsi untuk memproses data JSON
 def Convert_Json(data_json): 
@@ -420,4 +422,5 @@ def get_solution():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
