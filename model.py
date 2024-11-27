812c774c9b445a23e70a667604b597f72e3e715d
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-11-27 17:53:06 +0700

    Update model.py

--- model.py
+++ model.py
@@ -4,22 +4,26 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from collections import OrderedDict
 import requests
-app = Flask(__name__)
-api = Api(app)
 import os
 from dotenv import load_dotenv
+app = Flask(__name__)
+api = Api(app)
+
+
+
 
 # Fungsi untuk memproses data JSON
-def Convert_Json(data_json): 
+def Convert_Json(data_json):
     Nama_Jalan = [item.get('Street', '') for item in data_json['data']]
     Kota = [item.get('City', '') for item in data_json['data']]
     Provinsi = [item.get('Province', '') for item in data_json['data']]
     Kapasitas = [int(item.get('Kg', 0)) for item in data_json['data']]
     Kode_Pos = [item.get('Postal_Code', '') for item in data_json['data']]
-    Banyak_Kendaraan  = data_json['Number_of_vehicles']
-
+    Banyak_Kendaraan = data_json['Number_of_vehicles']
+    Kapasitas[0] = 0
     Addresses = [f"{a},{b},{c}" for a, b, c in zip(Nama_Jalan, Kota, Provinsi)]
-    return Addresses, Kapasitas, Banyak_Kendaraan,Nama_Jalan,Kota,Provinsi,Kode_Pos
+    return Addresses, Kapasitas, Banyak_Kendaraan, Nama_Jalan, Kota, Provinsi, Kode_Pos
+
 
 def calculate_distance_matrix(origins, destinations, api_key, mode="driving"):
     url = "https://maps.googleapis.com/maps/api/distancematrix/json"
@@ -83,6 +87,7 @@ def geocode_addresses(addresses, api_key):
             results.append({"address": address, "error": f"HTTP Error: {response.status_code}"})
 
     return results, coordinates, Latitude, Longitude
+
 
 # matrix, origins, destinations = calculate_distance_matrix(coordinates, coordinates, api_key)
 
@@ -185,6 +190,7 @@ def pembentukan_rute_vrp(permutasi_birds, data):
 
     return routes
 
+
 def menghitung_jarak_per_rute(routes, data):
     """
     Menghitung total jarak tempuh setiap rute (kendaraan) untuk setiap bird.
@@ -216,6 +222,7 @@ def menghitung_jarak_per_rute(routes, data):
 
     return all_birds_distances
 
+
 def menghitung_jarak_total_birds(all_birds_distances):
     """
     Menghitung total jarak tempuh untuk setiap birds (fungsi tujuan).
@@ -227,6 +234,7 @@ def menghitung_jarak_total_birds(all_birds_distances):
         total_distances.append(total_distance)  # Simpan total jarak untuk bird ini
 
     return total_distances
+
 
 # Fungsi: Lévy Flight
 def levy_flight(mean):
@@ -244,7 +252,8 @@ def replace_bird(position, alpha_value, lambda_value, min_values, max_values, ob
     levy_values = levy_flight(lambda_value)
     new_solution = np.copy(position[random_bird, :-1])
     rand_factors = np.random.rand(len(min_values))
-    new_solution = np.clip(new_solution + alpha_value * levy_values * new_solution * rand_factors, min_values, max_values)
+    new_solution = np.clip(new_solution + alpha_value * levy_values * new_solution * rand_factors, min_values,
+                           max_values)
     new_fitness = objective_function(np.argsort(new_solution).astype(int) + 1)
     if new_fitness < position[random_bird, -1]:
         position[random_bird, :-1] = new_solution
@@ -263,9 +272,12 @@ def update_positions(position, discovery_rate, min_values, max_values, objective
     for i in nest_list:
         rand = np.random.rand(updated_position.shape[1] - 1)
         if np.random.rand() > discovery_rate:
-            updated_position[i, :-1] = np.clip(updated_position[i, :-1] + rand * (updated_position[bird_j, :-1] - updated_position[bird_k, :-1]), min_values, max_values)
+            updated_position[i, :-1] = np.clip(
+                updated_position[i, :-1] + rand * (updated_position[bird_j, :-1] - updated_position[bird_k, :-1]),
+                min_values, max_values)
     updated_position[:, -1] = [objective_function(np.argsort(bird[:-1]).astype(int) + 1) for bird in updated_position]
     return updated_position
+
 
 def cetak_hasil(routes, data, all_birds_distances):
     """
@@ -295,7 +307,7 @@ def cetak_hasil(routes, data, all_birds_distances):
         #     if customer != data['depot'] :
         #         Lat.append(Latitude[customer])
         #         Long.append(Longitude[customer])
-                
+
         print(f"Route for vehicle {vehicle_idx}:")
         print(f"  {route_str}")
         print(f"  Distance of the route: {route_distance:.2f}m")
@@ -309,8 +321,6 @@ def cetak_hasil(routes, data, all_birds_distances):
 
     print(f"\nTotal Distance of all routes: {total_distance:.2f}m")
     print(f"Total Load of all routes: {total_load}")
-
-
 
 
 # Fungsi Utama: VRP Cuckoo Search
@@ -330,19 +340,21 @@ def vrp_cuckoo_search(data, birds=10, iterations=100, alpha_value=0.01, lambda_v
 
     best_individual = population[population[:, -1].argsort()][0, :]
     for count in range(iterations):
-        #print(f"\n=== Iterasi {count + 1} ===")
-        #print(f"Total Jarak (Fitness): {best_individual[-1]}")
+        # print(f"\n=== Iterasi {count + 1} ===")
+        # print(f"Total Jarak (Fitness): {best_individual[-1]}")
 
         # Cetak rute per kendaraan dari solusi terbaik saat ini
         best_permutation = [int(p) + 1 for p in np.argsort(best_individual[:-1])]
         routes = pembentukan_rute_vrp([best_permutation], data)[0]
         all_birds_distances = menghitung_jarak_per_rute([routes], data)
-        #cetak_hasil(routes, data, all_birds_distances)
+        # cetak_hasil(routes, data, all_birds_distances)
 
         # Lévy flights dan replace bird
         for _ in range(birds):
-            population = replace_bird(population, alpha_value, lambda_value, [0] * banyak_pelanggan, [1] * banyak_pelanggan, objective_function)
-        population = update_positions(population, discovery_rate, [0] * banyak_pelanggan, [1] * banyak_pelanggan, objective_function)
+            population = replace_bird(population, alpha_value, lambda_value, [0] * banyak_pelanggan,
+                                      [1] * banyak_pelanggan, objective_function)
+        population = update_positions(population, discovery_rate, [0] * banyak_pelanggan, [1] * banyak_pelanggan,
+                                      objective_function)
         current_best = population[population[:, -1].argsort()][0, :]
         if best_individual[-1] > current_best[-1]:
             best_individual = np.copy(current_best)
@@ -365,7 +377,7 @@ def get_solution():
         return jsonify({"error": "Invalid JSON"}), 400
 
     # Memproses data JSON
-    addresses, Kapasitas, Banyak_Kendaraan,Nama_Jalan, Kota, Provinsi ,Kode_Pos= Convert_Json(data_input)
+    addresses, Kapasitas, Banyak_Kendaraan, Nama_Jalan, Kota, Provinsi, Kode_Pos = Convert_Json(data_input)
 
     # API Key Anda
     api_key = "AIzaSyB1byYrh8YOO9uLcnQMjUOSN8AjTTiOw58"
@@ -381,7 +393,7 @@ def get_solution():
         data = {}
         data['distance_matrix'] = matrix
         data['demands'] = Kapasitas
-        data["vehicle_capacities"] = 500  # Ini disesuaikan
+        data["vehicle_capacities"] = 120  # Ini disesuaikan
         data["num_vehicles"] = Banyak_Kendaraan
         data["depot"] = 0
         return data
@@ -399,16 +411,37 @@ def get_solution():
     for sol in final_routes:
         Kendaraan += 1
         Rute = []
+
+        Rute.append({
+            "latitude": Latitude[0],
+            "longitude": Longitude[0],
+            "street": Nama_Jalan[0],
+            "city": Kota[0],
+            "province": Provinsi[0],
+            "postal_code": Kode_Pos[0],
+            "demand": Kapasitas[0]
+        })
+
         for pelanggan in sol:
             Location_Data = {
-                    "latitude": Latitude[pelanggan],
-                    "longitude": Longitude[pelanggan],
-                    "street": Nama_Jalan[pelanggan],
-                    "city": Kota[pelanggan], 
-                    "province": Provinsi[pelanggan],
-                    "postal_code" :Kode_Pos[pelanggan]
-                }
+                "latitude": Latitude[pelanggan],
+                "longitude": Longitude[pelanggan],
+                "street": Nama_Jalan[pelanggan],
+                "city": Kota[pelanggan],
+                "province": Provinsi[pelanggan],
+                "postal_code": Kode_Pos[pelanggan],
+                "demand": Kapasitas[pelanggan]
+            }
             Rute.append(Location_Data)
+        Rute.append({
+            "latitude": Latitude[0],
+            "longitude": Longitude[0],
+            "street": Nama_Jalan[0],
+            "city": Kota[0],
+            "province": Provinsi[0],
+            "postal_code": Kode_Pos[0],
+            "demand": Kapasitas[0]
+        })
         Hasil[f"Kendaraan{Kendaraan}"] = Rute
 
     solution = OrderedDict({
@@ -419,9 +452,6 @@ def get_solution():
     # Mengembalikan solusi dalam format JSON
     return jsonify(solution)
 
-
-# Load variabel dari file .env
-load_dotenv()  #
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
